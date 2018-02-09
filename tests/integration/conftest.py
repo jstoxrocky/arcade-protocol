7ab418319c175369d63656cc96db0012aaabe95a
jstoxrocky <joestox@gmail.com>
2018-02-09 17:18:45 -0500

    Add Rinkeby inmtegration test for adjustPrice

---
+++ tests/integration/conftest.py
@@ -0,0 +1,54 @@
+import pytest
+import os
+import json
+from web3.providers import (
+    HTTPProvider,
+)
+from web3 import (
+    Web3,
+    Account,
+)
+from eth_utils import (
+    to_wei,
+    keccak,
+)
+
+
+ABI = '[{"constant": true, "inputs": [{"name": "", "type": "uint256"}, {"name": "", "type": "address"}], "name": "participants", "outputs": [{"name": "", "type": "bool"}], "payable": false, "stateMutability": "view", "type": "function"}, {"constant": true, "inputs": [], "name": "round", "outputs": [{"name": "", "type": "uint256"}], "payable": false, "stateMutability": "view", "type": "function"}, {"constant": false, "inputs": [], "name": "pay", "outputs": [], "payable": true, "stateMutability": "payable", "type": "function"}, {"constant": true, "inputs": [], "name": "percentFee", "outputs": [{"name": "", "type": "uint256"}], "payable": false, "stateMutability": "view", "type": "function"}, {"constant": true, "inputs": [], "name": "jackpot", "outputs": [{"name": "", "type": "uint256"}], "payable": false, "stateMutability": "view", "type": "function"}, {"constant": true, "inputs": [], "name": "owner", "outputs": [{"name": "", "type": "address"}], "payable": false, "stateMutability": "view", "type": "function"}, {"constant": true, "inputs": [], "name": "price", "outputs": [{"name": "", "type": "uint256"}], "payable": false, "stateMutability": "view", "type": "function"}, {"constant": false, "inputs": [{"name": "h", "type": "bytes32"}, {"name": "v", "type": "uint8"}, {"name": "r", "type": "bytes32"}, {"name": "s", "type": "bytes32"}, {"name": "_addr", "type": "address"}, {"name": "score", "type": "uint256"}], "name": "uploadScore", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function"}, {"constant": true, "inputs": [{"name": "_addr", "type": "address"}], "name": "getParticipation", "outputs": [{"name": "", "type": "bool"}], "payable": false, "stateMutability": "view", "type": "function"}, {"constant": false, "inputs": [{"name": "h", "type": "bytes32"}, {"name": "v", "type": "uint8"}, {"name": "r", "type": "bytes32"}, {"name": "s", "type": "bytes32"}, {"name": "_addr", "type": "address"}, {"name": "_price", "type": "uint256"}], "name": "adjustPrice", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function"}, {"inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor"}]'  # noqa: E501
+ADDRESS = '0x2E26Cc06B0aB12C01a7F8D805d95c14D5D886f96'
+
+
+@pytest.fixture(scope="module")
+def rinkeby_address():
+    return ADDRESS
+
+
+@pytest.fixture(scope="module")
+def web3():
+    infura_token = os.environ['INFURA_ACCESS_TOKEN']
+    provider = HTTPProvider('https://rinkeby.infura.io/%s' % (infura_token))
+    return Web3(provider)
+
+
+@pytest.fixture(scope="function")
+def contract(web3):
+    contract = web3.eth.contract(
+        abi=json.loads(ABI),
+        address=ADDRESS,
+    )
+    return contract
+
+
+@pytest.fixture(scope="module")
+def owner(web3):
+    privkey = os.environ['ARCADE_PRIVATE_KEY']
+    owner = Account.privateKeyToAccount(privkey)
+    return owner
+
+
+@pytest.fixture(scope="module")
+def user(web3):
+    privkey = keccak('1')
+    user = Account.privateKeyToAccount(privkey)
+    assert web3.eth.getBalance(user.address) >= to_wei(1, 'ether')
+    return user
---
+++ tests/integration/test_rinkeby.py
@@ -0,0 +1,81 @@
+import pytest
+import requests
+from web3 import (
+    Account,
+)
+from eth_utils import (
+    decode_hex,
+)
+from contracts import (
+    sign,
+)
+from web3.utils.transactions import (
+    wait_for_transaction_receipt,
+)
+
+
+def test_contract_has_address(contract, rinkeby_address):
+    assert contract.address == rinkeby_address
+
+
+def test_contract_has_code(web3, contract):
+    assert web3.eth.getCode(contract.address) != b''
+
+
+def test_contract_owner_is_from_envvar(web3, contract, owner):
+    contract_owner = contract.functions.owner().call()
+    assert contract_owner == owner.address
+
+
+def test_signer_is_contract_owner(web3, contract, user):
+    data = dict(user=user.address)
+    result = requests.get('http://127.0.0.1:5000/price', params=data)
+    data_from_endpoint = result.json()
+    signed = data_from_endpoint['signature']
+    signer = Account.recover(
+        signed['messageHash'],
+        signature=signed['signature'],
+    )
+    contract_owner = contract.functions.owner().call()
+    assert contract_owner == signer
+
+
+def test_contract_address_same_as_webserver(web3, contract, owner, user):
+    # Since all owner tests pass, and we have price, user
+    # If this test fails, then it is the contract address
+    data = dict(user=user.address)
+    result = requests.get('http://127.0.0.1:5000/price', params=data)
+    data_from_endpoint = result.json()
+    price = data_from_endpoint['price']
+    signed = data_from_endpoint['signature']
+    self_signed = sign(owner.privateKey, contract.address, user.address, price)
+    assert signed['signature'] == self_signed['signature']
+
+
+@pytest.mark.skip
+def test_price(web3, contract, user):
+    data = dict(user=user.address)
+    result = requests.get('http://127.0.0.1:5000/price', params=data)
+    data_from_endpoint = result.json()
+    price = data_from_endpoint['price']
+    signed = data_from_endpoint['signature']
+
+    tx = contract.functions.adjustPrice(
+        signed['messageHash'],
+        signed['v'],
+        decode_hex(signed['r']),
+        decode_hex(signed['s']),
+        user.address,
+        price,
+    ).buildTransaction({
+        'from': user.address,
+        'gas': 4000000,
+        'gasPrice': 2000000000,
+        'nonce': web3.eth.getTransactionCount(user.address),
+    })
+    signed_tx = Account.signTransaction(tx, user.privateKey)
+    tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+    receipt = wait_for_transaction_receipt(web3, tx_hash)
+    assert receipt is not None
+    assert bool(receipt.status)
+    assert contract.functions.price().call() == price
