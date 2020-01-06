d296569a565e2070ecbb39962619805c0c417359
jstoxrocky <joestox@gmail.com>
2020-01-06 17:55:37 +0100

    Switch to abi.encode_packed

--- contracts/__init__.py
+++ contracts/__init__.py
@@ -8,15 +8,8 @@ from toolz.dicttoolz import (
 from eth_account.messages import (
     encode_intended_validator,
 )
-from web3._utils.encoding import (
-    hex_encode_abi_type,
-)
-from eth_utils import (
-    add_0x_prefix,
-    remove_0x_prefix,
-)
-from eth_typing import (
-    HexStr,
+from eth_abi.packed import (
+    encode_abi_packed,
 )
 
 
@@ -27,21 +20,16 @@ CONTRACTS_DIR = os.path.join(_outer_dir_abs, 'solidity')
 BIN_DIR = os.path.join(_outer_dir_abs, 'bin')
 
 
-def sign(private_key, contract, user, *values):
-    # This should be a function
+def sign_score(private_key, contract, user, score):
     abi_types = ['address', 'uint256']
-    values = [user, values[0]]
-    message = add_0x_prefix(HexStr(''.join(
-        remove_0x_prefix(hex_encode_abi_type(abi_type, value))
-        for abi_type, value
-        in zip(abi_types, values)
-    )))
+    values = [user, score]
+    message = encode_abi_packed(abi_types, values)
     eip191_message = encode_intended_validator(
         validator_address=contract,
-        hexstr=message,
+        primitive=message,
     )
     # sign_message passes data through
-    # eth_account.messages._hash_eip191_message
+    # eth_account.messages._hash_eip191_message which is keccak256
     signed = Account.sign_message(eip191_message, private_key)
     signed = valmap(lambda x: x.hex() if isinstance(x, bytes) else x, signed)
     return signed
